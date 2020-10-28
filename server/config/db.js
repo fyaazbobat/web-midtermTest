46980cc2956b3e8623035bd43900d2b30533d7aa
fbobat <71932173+fbobat@users.noreply.github.com>
2020-10-28 18:52:13 -0400

    final version of midterm test

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://fyaaz:fyaaz@web-midterm.ewpvq.mongodb.net/fyaaz?retryWrites=true&w=majority"
+};
